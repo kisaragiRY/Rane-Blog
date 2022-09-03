4d1d396d059efc960a41c2a1d52c02f9548e9f9c
Kisaragi <kisaragi.z@hacarus.com>
2022-09-03 22:58:24 +0900

    update footer and about page design

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../Footer/footer.css"
-import { useLocation} from 'react-router-dom'
+import { useLocation,Link} from 'react-router-dom'
 
 
 export default function Footer() {
@@ -9,11 +9,28 @@ export default function Footer() {
     
     <div className='footer'>
       
-      {location.pathname==='/'&&
+      {(location.pathname==='/')| (location.pathname==='/about') &&
         <div className='home-footer'>
-          <hr/>
-          <h3>Rane's blog</h3>
-          <p>A blog that's about </p>
+          {/* <img src='https://res.cloudinary.com/kisaragiry/image/upload/v1661079193/raneblog.com/Untitled_64_o5gmom.png'></img> */}
+          <div className='home-footer-left'>
+            <h3>Rane's blog</h3>
+            <p>This this Rane's official blog where she writes about 
+              Math, Statistics, Machine Learning and other random thoughts.</p>
+          </div>
+
+          <div className='home-footer-right'>
+            <div className='contact'>
+              <h3>get in touch</h3>
+              <p><a href='https://www.instagram.com/kisaragi.z/' target="_blank">instagram</a></p>
+              <p><a href='https://github.com/kisaragiRY' target="_blank">github</a></p>
+            </div>
+
+            <div className='nav'>
+              <h3>explore</h3>
+              <p><Link to='/post'>post</Link></p>
+            </div>
+          </div>
+          
         </div>
       }
       <span>Copyright © 2022 Rane.Z</span>
--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -39,32 +39,79 @@ export default function About() {
       <div className='about-title'>
         <span>About Me</span>
       </div>
-      <div className='about-post'>
-        <div className='about-markdown'>
+      <div className='about-me'>
+        {/* <div className='about-markdown'>
           <Markdown content={post} />
-        </div>
-        <div className='contact-form'>
-        <div className="contact-header"> contact me</div>
+        </div> */}
+        <div className='que-ans'>
+          <div className='que'>Who are you?</div>
+          <div className='ans'>
+              <p>My name is <mark>Rane</mark>, currently a DS master student 
+                focusing on all kinds of <mark>machine learning algorithms</mark> and
+                of course <mark>statistical learning</mark>.
+              </p>
+          </div>
 
-          <form className="contact-body" ref={form} onSubmit={sendEmail}>
-            <input name="name" type="text" className="contact-body__name" required placeholder="Name"/>
-            <input name="email" type="email" className="contact-body__email" placeholder="Email" />
-            <select name='Continent' className="contact-body__continent">
-              <option value="">--Please choose a place where you are from--</option>
-              <option>Africa</option>
-              <option>Antarctica</option>
-              <option>Asia</option>
-              <option>Australia</option>
-              <option>Europe</option>
-              <option>North America</option>
-              <option>South America</option>
-            </select>
-            <textarea name='message' type="text" className="contact-body__message" placeholder="Message" required></textarea>
-            <button value="Send" className="contact-body__submit" type="submit">SEND</button>
-            <span className={Msent?'sent-message active':'sent-message'}>Thank you, message sent!</span>
+          <div className='que'>What is this website about?</div>
+          <div className='ans'>
+              <p>
+                This website is my personal blog where I write about about <mark>Math, 
+                Statistics, Machine Learning and other random stuff</mark>.
+          
+              </p>
+          </div>
 
-          </form>
+          <div className='que'>What are you busy with recently?</div>
+          <div className='ans'>
+              <p>
+                I'm in the last year of my master program, so school stuff is 
+                my priority. 
+              </p>
+              <p>On the side, I write blogs, furnishing my website and learning French 🇫🇷.
+
+              </p>
+          </div>
+
+          <div className='que'>What do you do for fun &#x1F60B;?</div>
+          <div className='ans'>
+              <p>
+                I design houses using an app called shaper3D (no ads here) and sometimes
+                do pixel art (<mark><a href='https://www.instagram.com/kisaragi.z/'>link to my instagram</a></mark>).
+              </p>
+          </div>
+
+          <div className='que'>How do I contact you?</div>
+          <div className='ans'>
+              <p>
+                If you have questions about the blogs, and want to have discussion further, 
+                you can write emails to <mark>rane.z.blog@gmail.com</mark>.
+              </p>
+          </div>
+          
         </div>
+
+        {/* <div className='contact-form'>
+          <div className="contact-header"> contact me</div>
+
+            <form className="contact-body" ref={form} onSubmit={sendEmail}>
+              <input name="name" type="text" className="contact-body__name" required placeholder="Name"/>
+              <input name="email" type="email" className="contact-body__email" placeholder="Email" />
+              <select name='Continent' className="contact-body__continent">
+                <option value="">--Please choose a place where you are from--</option>
+                <option>Africa</option>
+                <option>Antarctica</option>
+                <option>Asia</option>
+                <option>Australia</option>
+                <option>Europe</option>
+                <option>North America</option>
+                <option>South America</option>
+              </select>
+              <textarea name='message' type="text" className="contact-body__message" placeholder="Message" required></textarea>
+              <button value="Send" className="contact-body__submit" type="submit">SEND</button>
+              <span className={Msent?'sent-message active':'sent-message'}>Thank you, message sent!</span>
+
+            </form>
+          </div> */}
       </div>
 
     </div>
