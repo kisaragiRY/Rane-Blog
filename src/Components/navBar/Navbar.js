08ddc78057dfbfac56df74053080dd1d106ca36c
Kisaragi <none>
2022-12-15 22:17:40 +0900

    finished changing the nacbar design #1

--- src/Components/navBar/Navbar.js
+++ src/Components/navBar/Navbar.js
@@ -4,47 +4,40 @@ import './navbar.css'
 
 export default function Navbar() {
     const location=useLocation();
-    const [navBg,setNavBg]=useState();
-    const [navLinks,setNavLinks]=useState();
-    const [navBorder,setBorder]=useState();
     const [isActive,setActive]=useState(false);
 
     useEffect(()=>{
         const homeUrl=process.env.PUBLIC_URL;
-        if(window.location.pathname===homeUrl|window.location.pathname===homeUrl+'/'){
-            setNavBg("var(--color-red)");
-            setNavLinks("var(--color-bg)");
-            setBorder("solid 0px");
-        }
-        else{
-            setNavBg("var(--color-bg)");
-            setNavLinks("var(--color-text)");
-            setBorder("solid 1px");
-        }
+        // if(window.location.pathname===homeUrl|window.location.pathname===homeUrl+'/'){
+        //     setNavBg("var(--color-text)");
+        //     setBorder("solid 0px");
+        // }
+        // else{
+        //     setNavBg("var(--color-bg)");
+        //     setBorder("solid 1px");
+        // }
     },[location]);
     const toggleClass=()=>{
         setActive(!isActive);
     }
-    const navStyle={backgroundColor:navBg,borderBottom:navBorder,zIndex:'100'}
-    const navMenuBgcolor={backgroundColor:navBg}
-    const Linkcolor={backgroundColor:navBg,color:navLinks};
 
   return (
-    <div className='navbar-container' style={navStyle}>
-    <nav className='navbar' >
-        <div className='navbar-name'>
-            <li><Link to='/' style={Linkcolor}>Home</Link></li> 
+    <div className='navbar-container'>
+    
+        <div className='navbar-logo'>
+            <li><Link to='/'>Home</Link></li> 
         </div>
-        <div className={isActive?'navbar-menu-links is-active':'navbar-menu-links'}  style={navMenuBgcolor}>
-        <li ><Link to='/post'  style={Linkcolor} onClick={()=>{setActive(!isActive);}}>Post</Link></li>
-        <li><Link to='/about' style={Linkcolor} onClick={()=>{setActive(!isActive);}}>About</Link></li>
+        <div className={isActive?'navbar-menu-links is-active':'navbar-menu-links'} >
+        <li ><Link to='/post'   onClick={()=>{setActive(!isActive);}}>Post</Link></li>
+        <li><Link to='/' onClick={()=>{setActive(!isActive);}}>Service</Link></li>
+        <li><Link to='/about' onClick={()=>{setActive(!isActive);}}>About</Link></li>
         </div>
-        <button className={isActive?'hamburger is-active':'hamburger'} style={navMenuBgcolor} onClick={toggleClass}>
-            <span style={{backgroundColor:navLinks}}></span>
-            <span style={{backgroundColor:navLinks}}></span>
-            <span style={{backgroundColor:navLinks}}></span>
+        <button className={isActive?'hamburger is-active':'hamburger'} onClick={toggleClass}>
+            <span></span>
+            <span></span>
+            <span></span>
         </button>
-    </nav>
+
     </div>
   )
 }
