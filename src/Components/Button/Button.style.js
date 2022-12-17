3d7f6305387074e5161573a29786c21c7fc6a34a
Kisaragi <none>
2022-12-17 21:50:40 +0900

    changed button components styles #1

--- src/Components/Button/Button.style.js
+++ src/Components/Button/Button.style.js
@@ -3,16 +3,16 @@ import styled from "styled-components";
 export const Button =styled.button`
   font-size: ${(props)=>props.font_size};;
   box-sizing: border-box;
-  border: 1px solid var(--color-text);
+  border: 2px solid var(--color-text);
   margin: 5px;
   padding: ${(props)=>props.padding};
   color: var(--color-text);
   letter-spacing: .05em;
-  background-color:white;
+  font-weight: bold;
+  background-color: var(--color-bg);
   &:hover{
-  cursor: pointer;
-  background-color: var(--color-red);
-  color: white;
-  transition: 0.1s;
+    cursor: pointer;
+    background-color: var(--color-red);
+    transition: 0.1s;
   }
 `
