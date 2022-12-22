162e06ed5720375fbfab3a31cc3ee4f3e9fa05da
Kisaragi <none>
2022-12-22 20:39:21 +0900

    added service window #1

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,11 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
+import { useNavigate} from 'react-router-dom'
 import './home.css'
 import blogs from '../../Blogs/blogs.json'
 import { sort_json } from '../../Helpers/utils'
 import { Link } from 'react-router-dom'
 import Window from '../../Components/Window/window'
+import { Button } from '../../Components/Button/Button.style'
 
 export default function Home() {
+  const [clickCount, setClickCount] = useState(1)
+  let navigate=useNavigate();
+
   const recent_ariticle_content = (
     <div className='recent-article'>
       <h2>Recent Articles</h2>
@@ -24,7 +29,7 @@ export default function Home() {
   )
 
   const about_me_content = (
-    <div className='about-me'>
+    <div className='home-about'>
       <div className='bio'>
         <Link to='/about' >
           <img src='https://res.cloudinary.com/kisaragiry/image/upload/v1671538294/raneblog.com/lady_prof_j6vrwu.svg'/>
@@ -48,6 +53,23 @@ export default function Home() {
     </div>
   )
 
+  const service_content = (
+    <div className='home-service'>
+      <h1>Find Solutions</h1>
+      <span>Boost your business with AI. Get insights through data analysis.</span>
+      <Button 
+        font_size="1.2rem" 
+        padding=".5rem"
+        border_thickness="3px"
+        onClick={()=>{
+          let path=`/`;
+          navigate(path)}}
+        >
+          Learn More
+      </Button>
+    </div>
+  )
+
   return (
     <div className='home-wrapper'>
         <div className='home-intro'>
@@ -57,27 +79,37 @@ export default function Home() {
             <span>Rane's Blog</span>
             <div className='quick-links-wrapper'>
               <li ><Link to='/post' >Post</Link></li>
+              <li><Link to='/' >Service</Link></li>
               <li><Link to='/about' >About</Link></li>
             </div>
         </div>
-        <div className='interface' >
+        <div className='interface' onClick={()=>{setClickCount(clickCount+1)}}>
 
           <Window 
             window_width = '30rem'
             window_name = 'about_me.html'
             window_content = {about_me_content}
-            window_start_top = '10vw'
-            window_start_left = '46vw'
-
+            window_start_top = '28vw'
+            window_start_left = '10vw'
+            total_count = {clickCount}
             />
 
           <Window 
             window_width = '25rem'
             window_name = 'recent_article.html'
             window_content = {recent_ariticle_content}
-            window_start_top = '5vw'
-            window_start_left = '20vw'
-   
+            window_start_top = '3vw'
+            window_start_left = '50vw'
+            total_count = {clickCount}
+            />
+
+          <Window 
+            window_width = '25rem'
+            window_name = 'service.html'
+            window_content = {service_content}
+            window_start_top = '3vw'
+            window_start_left = '10vw'
+            total_count = {clickCount}
             />
         
         </div>
