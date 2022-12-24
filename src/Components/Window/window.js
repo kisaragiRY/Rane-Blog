e296004967568e965b10e45ce7ed3de408112d7c
Kisaragi <none>
2022-12-24 21:20:12 +0900

    added the button click display #1

--- src/Components/Window/window.js
+++ src/Components/Window/window.js
@@ -1,35 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Draggable from "react-draggable";
 import './window.css';
 
 export default function Window(props){
     const [clickCount, setClickCount] = useState(1)
-    const OnMouseClick = () => {
-            setClickCount(props.total_count+1)
-        }
-    const window_style = {
+
+    var window_style = {
         width: props.window_width,
         top: props.window_start_top,
         left: props.window_start_left,
-        zIndex: clickCount
+        zIndex: clickCount,
     }
 
+    const window_click_handler = () => {
+        if(props.total_count){
+            setClickCount(props.total_count+1)
+        }
+    }
 
+    const dot1_click_handler = () =>{
+        if(props.window_display){
+            props.window_display("none")
+        }
+    }
+    
     return(
         <Draggable>
-        <div className='window-wrapper' style = {window_style} onClick={OnMouseClick}>
+        <div  id={props.id} className='window-wrapper' style = {window_style} onClick={window_click_handler}>
             <div className='window-header' >
-                <span>
-                    <img src='https://res.cloudinary.com/kisaragiry/image/upload/v1671362504/raneblog.com/circle_ktldki.svg' />
-                    <img src='https://res.cloudinary.com/kisaragiry/image/upload/v1671362504/raneblog.com/circle_ktldki.svg' />
-                </span>
+                <div className='header-button'>
+                    <span className='dot' onClick={dot1_click_handler}></span>
+                    <span className='dot'></span>
+                </div>
                 <span>{props.window_name}</span>
             </div>
             <div className='window-content'>
                 {props.window_content}
             </div>
-
         </div>
         </Draggable>
+
     )
 }
