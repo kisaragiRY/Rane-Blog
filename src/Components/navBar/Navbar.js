1f10dcc9dfe4d54b00e8efee0a230abe31a8e096
kisaragi <azeuunghy@gmail.com>
2023-12-22 18:11:32 +0900

    applied transition to nav bar link

--- src/Components/navBar/Navbar.js
+++ src/Components/navBar/Navbar.js
@@ -1,11 +1,15 @@
 import React,{useState,useEffect} from 'react'
 import { useLocation,Link } from 'react-router-dom'
+
 import './hamburger.css'
 import { NavWrapper } from './NavWrapper.style';
+import { useTransitionNavigate } from '../../Helpers/Transition/useTransitionNavigate';
 
 export default function Navbar() {
-    const location=useLocation();
+    const location = useLocation();
     const [isActive,setActive]=useState(false);
+
+    const {transitionNavigate} = useTransitionNavigate();
 
     const toggleClass=()=>{
         setActive(!isActive);
@@ -13,12 +17,14 @@ export default function Navbar() {
   return (
     <NavWrapper isHomePage={location.pathname==="/"? true : false}>
         <div className='navbar-logo'>
-            <Link to='/'>Home</Link>
+            <a onClick={() => transitionNavigate("/", "slide-to-right")}>Home</a>
+
         </div>
         <div className={isActive?'navbar-menu-links is-active':'navbar-menu-links'} onClick={toggleClass}>
-            <Link to='/works'>works</Link>
-            <Link to='/post'>post</Link>
-            <Link to='/about'>about</Link>
+            <a onClick={() => transitionNavigate("/works", "slide-to-right")}>works</a>
+            <a onClick={() => transitionNavigate("/post", "slide-to-right")}>post</a>
+            <a onClick={() => transitionNavigate("/about", "slide-to-right")}>about</a>
+
         </div>
         <button className={isActive?'hamburger is-active':'hamburger'} onClick={toggleClass}>
             <span></span>
