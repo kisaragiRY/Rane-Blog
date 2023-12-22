ff624a73dfccbb94acd8d8441f98cf76646493e6
kisaragi <azeuunghy@gmail.com>
2023-12-22 22:53:39 +0900

    refined condition #12

--- src/Components/Button/TagButton.style.js
+++ src/Components/Button/TagButton.style.js
@@ -5,16 +5,18 @@ export const TagButton = styled.button`
   box-sizing: border-box;
   border: ${(props) => props.border_thickness} solid var(--color-text);
   border-radius: 1em;
-  margin: ${(props) => props.margin ? ".8em" : ".3em"};
-  padding: ${(props) => props.padding ? props.padding: "none"};
+  margin: ${(props) => props.margin ? props.margin : ".3em"};
+  padding: ${(props) => props.padding ? props.padding : "none"};
   /* color: var(--color-text); */
   letter-spacing: .05em;
   font-weight: bold;
-  background-color: var(--color-bg);
+  background-color: ${(props) => (props.bgColor==="red") ? "var(--color-red)" : "var(--color-bg)"};
+  color: ${(props) => (props.bgColor==="red") ? "white" : "var(--color-text)"};
+
   &:hover{
     cursor: pointer;
-    background-color: var(--color-red);
+    background-color: ${(props) => (props.bgColor=="red") ? "var(--color-bg)" : "var(--color-red)"};
     transition: 0.1s;
-    color: white;
+    color: ${(props) => (props.bgColor==="red") ? "var(--color-text)" : "white"};
   }
 `
