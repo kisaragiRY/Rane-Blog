2783283ae874bd3b96341331dec1f5f931ae6eb0
kisaragi <azeuunghy@gmail.com>
2023-12-22 22:53:03 +0900

    updated footer #12

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,38 +1,24 @@
 import React from 'react'
-import "../Footer/footer.css"
 import { useLocation,Link} from 'react-router-dom'
+import { BsYoutube, BsInstagram, BsGithub } from "react-icons/bs";
+import styled from 'styled-components';
+
+import "../Footer/footer.css"
 
 
 export default function Footer() {
-  let location=useLocation();
+  let location = useLocation();
   return (
     <div className='footer'>
-      {((location.pathname==='/') || (location.pathname==='/about') || (location.pathname==='/service')) &&
-        <div className='home-footer'>
-          <div className='home-footer-left'>
-            <h3>Get in touch</h3>
-              <p>rane.z.blog@gmail.com</p>
-              <p>website designed・developed by Rane.Z</p>
-          </div>
+      <div className='iconList'>
+          <a href='https://www.youtube.com/channel/UCxyic1BkqjQHIb1nuxpAOeg' target="_blank" rel="noreferrer"><BsYoutube /></a>
+          <a href='https://www.instagram.com/kisaragi.z/' target="_blank" rel="noreferrer"><BsInstagram/></a>
+          <a href='https://github.com/kisaragiRY' target="_blank" rel="noreferrer"><BsGithub/></a>
+      </div>
+      <div>
+        Copyright © 2020 - 2023, designed + developed by Isra.Z
+      </div>
 
-          <div className='home-footer-right'>
-            <div className='nav'>
-              <h3>explore</h3>
-              <p><Link to='/post'>Post</Link></p>
-              <p><Link to='/about'>About</Link></p>
-            </div>
-
-            <div className='contact'>
-              <h3>find me</h3>
-              <p><a href='https://www.youtube.com/channel/UCxyic1BkqjQHIb1nuxpAOeg' target="_blank" rel="noreferrer">YouTube</a></p>
-              <p><a href='https://www.instagram.com/kisaragi.z/' target="_blank" rel="noreferrer">Instagram</a></p>
-              <p><a href='https://github.com/kisaragiRY' target="_blank" rel="noreferrer">GitHub</a></p>
-            </div>
-
-          </div>
-        </div>
-      }
-      <span>Copyright © 2020 - 2023 Rane.Z</span>
     </div>
   
   )
