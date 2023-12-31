3496dccf4fed28031d0b36bd0ee4d2ea30516098
kisaragi <azeuunghy@gmail.com>
2023-12-31 20:15:07 +0900

    imported works page #16

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,3 +3,4 @@ export {default as Post} from './Post/post';
 export {default as PostDetail} from './PostDetail/PostDetail';
 export {default as About} from './About/about';
 export {default as Error} from './Error/Error';
+export { Works } from './Works/works'
