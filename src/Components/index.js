e39eb3af7e5a5df1b6a53c0c3806cba1cb026321
kisaragi <azeuunghy@gmail.com>
2023-12-31 20:15:59 +0900

    exported workcard component #16

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,5 @@
 export {default as Navbar} from './navBar/Navbar';
 export {default as Footer} from './Footer/Footer';
+
+export { TagButton } from './Button/TagButton.style'
+export { WorkCard } from './WorkCard/workCard.style'
