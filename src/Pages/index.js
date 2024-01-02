250d04b5bf9cbbe15b53e5d4f9ba6e0e9c18bd9e
kisaragi <azeuunghy@gmail.com>
2024-01-02 23:06:31 +0900

    commeted out post related pages #21

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,6 +1,6 @@
 export {default as Home} from './Home/home';
-export {default as Post} from './Post/post';
-export {default as PostDetail} from './PostDetail/PostDetail';
+// export {default as Post} from './Post/post';
+// export {default as PostDetail} from './PostDetail/PostDetail';
 export {default as About} from './About/about';
 export {default as Error} from './Error/Error';
 export { Works } from './Works/works'
