31ec403a4110fafd6393487ed023f3ae4cb4dc69
kisaragi <azeuunghy@gmail.com>
2024-01-02 22:48:40 +0900

    added skillbox #21

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export {default as Footer} from './Footer/Footer';
 
 export { TagButton } from './Button/TagButton.style'
 export { WorkCard } from './WorkCard/workCard.style'
+export { SkillBox } from './SkillBox/skillbox';
