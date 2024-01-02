eaa18726856354fec2a961825e3030d252db92c6
kisaragi <azeuunghy@gmail.com>
2024-01-02 22:46:58 +0900

    fixed footer to the bottom and added styled components #21

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,25 +1,32 @@
 import React from 'react'
-import { useLocation,Link} from 'react-router-dom'
+import { useLocation} from 'react-router-dom'
 import { BsYoutube, BsInstagram, BsGithub } from "react-icons/bs";
 import styled from 'styled-components';
 
-import "../Footer/footer.css"
+const FooterWrapper = styled.div`
+  padding: 1em 0;
+  text-align: center;
 
+.iconList a{
+    padding: 1em;
+    font-size: 1.5em;
+}
+`
 
 export default function Footer() {
   let location = useLocation();
   return (
-    <div className='footer'>
+    <FooterWrapper >
       <div className='iconList'>
           <a href='https://www.youtube.com/channel/UCxyic1BkqjQHIb1nuxpAOeg' target="_blank" rel="noreferrer"><BsYoutube /></a>
           <a href='https://www.instagram.com/kisaragi.z/' target="_blank" rel="noreferrer"><BsInstagram/></a>
           <a href='https://github.com/kisaragiRY' target="_blank" rel="noreferrer"><BsGithub/></a>
       </div>
       <div>
-        Copyright © 2020 - 2023, designed + developed by Isra.Z
+        Copyright © 2020 - 2024 Isra.Z
       </div>
 
-    </div>
+    </FooterWrapper >
   
   )
 }
